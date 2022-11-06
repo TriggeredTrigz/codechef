aabf10732f94f0328bde1e1fc4463998b59b0f9c
TriggeredTrigz <88758202+TriggeredTrigz@users.noreply.github.com>
2022-11-06 22:23:47 +0530

    files added
    Added py file for timetable and jpeg image used

---
+++ timetable_srm.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from PIL import ImageTk, Image
+#modules: tkinter, pillow
+
+def UI():
+    Timetable=Tk()
+    Timetable.geometry('1920x1080')
+    Timetable.state('zoomed')
+    Timetable.title("TimeTable")
+    Timetable['bg']='black'
+    tt_layout=Image.open('CollegeTimetable.jpg')
+    TTLayoutIMG=ImageTk.PhotoImage(tt_layout)
+    TTLayout=Label(image=TTLayoutIMG).place(relx=0.5,rely=0.5,anchor=CENTER)
+    Timetable.mainloop()
+
+UI()
+
